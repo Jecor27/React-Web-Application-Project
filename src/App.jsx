413163c07c32ba50cec096b66dd25470b46ea323
Jesus <jjcorrales76@gmail.com>
2025-02-26 11:28:25 -0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>Rick and morty bounty hunter list</h1>
+    </>
+  );
+}
+
+export default App;
