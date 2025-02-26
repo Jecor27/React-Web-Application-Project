b2195867d7d9fa6fc1ab4edcd31a532b19b9716b
Jesus <jjcorrales76@gmail.com>
2025-02-26 15:21:48 -0700

    added tags

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [character, setCharacter] = useState([]);
+  const [characters, setCharacter] = useState([]);
 
   useEffect(() => {
     const getCharacters = async () => {
       try {
         const response = await fetch(
-          "https://rickandmortyapi.com/api/character?page=3"
+          "https://rickandmortyapi.com/api/character?page=29"
         );
         const data = await response.json();
         console.log(data.results);
@@ -18,15 +18,43 @@ function App() {
         console.log(error);
       }
     };
-    getCharacters()
+    getCharacters();
   }, []);
 
   return (
     <div>
-      <h1>Rick and morty bounty hunter list</h1>
-      <div>
-        {character.map}
-      </div>
+      <h1>Rick and Morties bounties</h1>
+      <ol>
+        {characters.map((character) => {
+          return (
+            <div key={character.id}>
+              <article>
+                <div>
+                  <img src= {character.image} />
+                </div>
+                <div>
+                  <h2> {character.name} </h2>
+                  <h3>{character.gender}</h3>
+                  <span>
+                    {character.status} - {character.species}
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    Last Known Location: <br></br>
+                    {character.location.name}
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    Place of origin: <br></br> {character.origin.name}
+                  </span>
+                </div>
+              </article>
+            </div>
+          );
+        })}
+      </ol>
     </div>
   );
 }
@@ -49,4 +77,3 @@ export default App;
 //   </>
 // );
 // }
-
